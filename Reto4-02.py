b1bb37d8e166b1f7669b240f2489332b3e0a43a5
OneCode182 <86318393+OneCode182@users.noreply.github.com>
2022-05-31 11:00:50 -0500

    Create Reto4-02.py

---
+++ Reto4-02.py
@@ -0,0 +1,38 @@
+
+import json
+
+
+# Juegos disponibles
+input1 = input()
+juegos = json.loads(input1)
+
+# Juegos a consultar
+input2 = input()
+consultar = list(input2.split(" "))
+
+clasificados = {}
+
+# ------- Programa -------
+def clasificar():
+    for i in consultar:
+        for j in juegos:
+            if i == j:
+                clasificados.update({i : juegos[i]})
+
+
+def precioTotal():
+    return sum(clasificados.values())
+
+
+def juegosDisponibles():
+    return " ".join(clasificados.keys())
+
+
+
+# Procesar datos
+clasificar()
+
+# Salidas
+print(precioTotal())
+print(juegosDisponibles())
+
